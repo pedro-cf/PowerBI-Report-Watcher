34716b5685f5bd05e2d47ab13f669c92fed7b907
Pedro Fernandes <pedro.fernandes@armis.pt>
2019-03-07 12:02:56 +0000

    last 15 days

--- ReportWatcher.ps1
+++ ReportWatcher.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference= 'silentlycontinue'
 
 if (!(Get-Module -ListAvailable -Name MicrosoftPowerBIMgmt.Profile)) {
-	Write-Host "Required module MicrosoftPowerBIMgmt.Profile not found. Installing..."
+	Write-Host "RequiYellow module MicrosoftPowerBIMgmt.Profile not found. Installing..."
 	if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 	Install-Module -Name MicrosoftPowerBIMgmt.Profile
 	Clear-Host
@@ -28,6 +28,9 @@ $groups_uri = "https://api.powerbi.com/v1.0/myorg/groups"
 $groups = (Invoke-WebRequest -Uri $groups_uri -UseBasicParsing -Headers $auth_headers | ConvertFrom-Json).value
 
 function ShowFailedRefreshes {
+	Write-Host "Ignoring refreshes over 15 days ago."
+	Write-Host ""
+	$fails = 0
 	foreach($group in $groups)
 	{
 		$printWorkspace = 1
@@ -45,17 +48,20 @@ function ShowFailedRefreshes {
 				if ($refresh) {
 					if ($refresh.refreshType -eq "Scheduled") {
 						$status = $refresh.status
-						if ($status -eq "Failed") {
+						$date = [DateTime]::$refresh.endTime
+						$15daysAgo = ([datetime]::Today).AddDays(-15)
+						if ($status -eq "Failed" -and $date -ge $15daysAgo) {
+							$fails++
 							if ($printWorkspace -eq 1) {
-								Write-Host "Workspace: $($group.name) (ID: $($group.id))" -ForegroundColor Red -BackgroundColor Black
+								Write-Host "Workspace: $($group.name) (ID: $($group.id))" -ForegroundColor Yellow -BackgroundColor Black
 								$printWorkspace = 0
 							}
 							
-							Write-Host "`t$($report.name)" -ForegroundColor Red -BackgroundColor Black
-							Write-Host "`t`tReport ID: $($report.id)" -ForegroundColor Red -BackgroundColor Black
-							Write-Host "`t`tDataset ID: $($report.datasetId)" -ForegroundColor Red -BackgroundColor Black
-							Write-Host "`t`tLast Refresh Date: $($refresh.endTime)" -ForegroundColor Red -BackgroundColor Black
-							Write-Host "`t`tLast Refresh Status: $($refresh.status)" -ForegroundColor Red -BackgroundColor Black
+							Write-Host "`t$($report.name)" -ForegroundColor Yellow -BackgroundColor Black
+							Write-Host "`t`tReport ID: $($report.id)" -ForegroundColor Yellow -BackgroundColor Black
+							Write-Host "`t`tDataset ID: $($report.datasetId)" -ForegroundColor Yellow -BackgroundColor Black
+							Write-Host "`t`tLast Refresh Date: $($date)" -ForegroundColor Yellow -BackgroundColor Black
+							Write-Host "`t`tLast Refresh Status: $($status)" -ForegroundColor Yellow -BackgroundColor Black
 							Write-Host ""
 						}
 					}
@@ -64,6 +70,8 @@ function ShowFailedRefreshes {
 			
 		}
 	}
+	Write-Host "$($fails) failed refreshes in the past 15 days."
+	Write-Host ""
 }
 
 function ExportFailedRefreshes {
@@ -96,8 +104,8 @@ function ShowAllReports {
 					if ($refresh.refreshType -eq "Scheduled") {
 						$status = $refresh.status
 						if ($status -eq "Failed") {
-							Write-Host "`t`tLast Refresh Date: $($refresh.endTime)" -ForegroundColor Red -BackgroundColor Black
-							Write-Host "`t`tLast Refresh Status: $($refresh.status)" -ForegroundColor Red -BackgroundColor Black
+							Write-Host "`t`tLast Refresh Date: $($refresh.endTime)" -ForegroundColor Yellow -BackgroundColor Black
+							Write-Host "`t`tLast Refresh Status: $($refresh.status)" -ForegroundColor Yellow -BackgroundColor Black
 						} else {
 							Write-Host "`t`tLast Refresh Date: $($refresh.endTime)"
 							Write-Host "`t`tLast Refresh Status: $($refresh.status)"
@@ -122,12 +130,12 @@ function ExportAllReports {
 
 function PrintMenu
 {
-	cls
+	Clear-Host
 	Write-Host "====== Report Watcher ======"
 	Write-Host ""
-	Write-Host "1: Show Failed Refreshes."
+	Write-Host "1: Show Failed Refreshes (Last 15 days)."
 	Write-Host "2: Show All Reports."
-	Write-Host "3: Export Failed Refreshes"
+	Write-Host "3: Export Failed Refreshes (Last 15 days)"
 	Write-Host "4: Export All Reports."
 	Write-Host "5: Exit."
 	Write-Host ""
@@ -140,16 +148,16 @@ do
 	switch ($input)
 	{
 		'1' {
-				cls
+				Clear-Host
 				ShowFailedRefreshes
 		} '2' {
-				cls
+				Clear-Host
 				ShowAllReports
 		} '3' {
-				cls
+				Clear-Host
 				ExportFailedRefreshes
 		} '4' {
-				cls
+				Clear-Host
 				ExportAllReports
 		} '5' {
 				return
